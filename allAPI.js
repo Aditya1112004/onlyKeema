cc138cd7b8aec7f192af15a6460702328a84a49b
Aditya1112004 <103874753+Aditya1112004@users.noreply.github.com>
2023-02-23 19:12:47 +0530

    Add files via upload

---
+++ allAPI.js
@@ -0,0 +1,49 @@
+foodItem = [
+  {
+    img: "images/bg1.jpg",
+    name: "chicken keema roll",
+    price: "",
+  },
+  {
+    img: "images/bg2.jpg",
+    name: "dal khichadi",
+    price: "",
+  },
+  {
+    img: "images/bg3.jpg",
+    name: "paneer keema roll",
+    price: "",
+  },
+  {
+    img: "images/bg6.jpg",
+    name: "chicken keema sandwich",
+    price: "",
+  },
+  {
+    img: "images/bg4.jpg",
+    name: "paneer momos",
+    price: "",
+  },
+  {
+    img: "images/bg8.jpg",
+    name: "masala cheese omlet",
+    price: "",
+  },
+  {
+    img: "images/bg7.jpg",
+    name: "chicken sandwich",
+    price: "",
+  },
+  {
+    img: "images/bg5.jpg",
+    name: "chicken momos",
+    price: "",
+  },
+  {
+    img: "images/bg9.jpg",
+    name: "special sandwich",
+    price: "",
+  },
+];
+
+module.exports = foodItem;
